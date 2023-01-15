343c2738ed459c36a738d008c043492fea594885
Mona Lisa <origami.nova@gmail.com>
2023-01-15 12:32:21 +0100

    hometaskdone2

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+Console.WriteLine("enter any number");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter second number");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if(n>m)
+   Console.Write($"(this number){n}");
+
+else 
+   Console.Write($"(this number){m}");
