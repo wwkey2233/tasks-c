084cfb782edc23dcb9d9dfa25bf9b0b84e557eb5
Mona Lisa <origami.nova@gmail.com>
2023-01-15 13:30:22 +0100

    hometaskdone6

---
+++ task6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("enter any number");
+int n =Convert.ToInt32(Console.ReadLine());
+if(n%2==0)
+  Console.Write($"(thisis an even number){n}");
+else 
+  Console.Write("error.enter another number");
